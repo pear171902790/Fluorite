1114b7ac3b07ba8b7f8a342dc0a2360a4362bc36
Peter Lee <lic@shinetechchina.com>
2015-12-18 10:20:53 +0800

    倒萨

--- Fluorite/Fluorite.MobileSite/Content/index.js
+++ Fluorite/Fluorite.MobileSite/Content/index.js
@@ -8,25 +8,32 @@
     var rightWidth = $('.right').width();
     $('div.left').css('height', windowHeight + 200 + 'px');
     $('div.cover').css('width', windowWidth / 3 + 'px').css('height', windowHeight + 'px').css('left', (mainWidth - rightWidth) + 'px');
-    var setHeight = function ($obj, value, propertyNames) {
-        var finalValue = (value * windowWidth) / 320;
-        if (propertyNames) {
+    var setResponsiveValue = function ($obj, values, propertyNames) {
+        if (propertyNames && values) {
+            var finalValue = 0;
             for (var i = 0; i < propertyNames.length; i++) {
+                if (values[i]!==undefined) {
+                    finalValue = (values[i] * windowWidth) / 320;
+                }
                 $obj.css(propertyNames[i], finalValue + 'px');
             }
         }
     }
-    setHeight($('div.logo img'), 32,['height']);
-    setHeight($('div.item_text'), 50, ['height','line-height']);
-    setHeight($('div.foot_right_text'), 55, ['height', 'line-height']);
-    setHeight($('div.foot_logo'), 55, ['min-height']);
-    setHeight($('.left_search'), 40, ['height']);
-    setHeight($('.left_search_input'), 30, ['height']);
-    setHeight($('.left_search_input input'), 30, ['height']);
-    setHeight($('.left_menu'), 40, ['height', 'line-height']);
-    setHeight($('.search_button'), 30, ['height']);
-    setHeight($('div.content'), 15, ['margin-top', 'margin-bottom']);
-    setHeight($('div.item'), 10, ['margin-top', 'margin-bottom']);
+
+    setResponsiveValue($('body'), [15], ['font-size']);
+    setResponsiveValue($('div.head'), [5], ['padding-top']);
+    setResponsiveValue($('div.menu img'), [30], ['height']);
+    setResponsiveValue($('div.logo img'), [32],['height']);
+    setResponsiveValue($('div.item_text'), [50,45], ['height','line-height']);
+    setResponsiveValue($('div.foot_right_text'), [55,47.5], ['height', 'line-height']);
+    setResponsiveValue($('div.foot_logo'), [55], ['min-height']);
+    setResponsiveValue($('.left_search'), [40], ['height']);
+    setResponsiveValue($('.left_search_input'), [30], ['height']);
+    setResponsiveValue($('.left_search_input input'), [30], ['height']);
+    setResponsiveValue($('.left_menu'), [40], ['height', 'line-height']);
+    setResponsiveValue($('.search_button'), [30], ['height']);
+    setResponsiveValue($('div.content'), [15], ['margin-top', 'margin-bottom']);
+    setResponsiveValue($('div.item'), [10], ['margin-top', 'margin-bottom']);
 
     var showLeftMenu = function () {
         window.scrollTo(0, 0);
